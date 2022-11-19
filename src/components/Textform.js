c5559cf775c73a7be88b71d4c212ee962b7ee3df
khushboo <khushboobagoria09@gmail.com>
2022-11-19 21:49:12 +0100

    inital commit

---
+++ src/components/Textform.js
@@ -0,0 +1,55 @@
+import React, {useState} from "react"
+
+
+function TextForm(props){
+   
+   const UpperClick =()=>{
+   
+    // console.log("uppercase button was clicked");
+   let newText=Text.toUpperCase();
+    setText(newText);
+   }
+   const LowerClick=()=>{
+    let newText=Text.toLowerCase();
+    setText(newText);
+   }
+   const HandleOnChange =(event)=>{
+    // console.log("on change");
+    setText(event.target.value);  
+   
+   }
+   const ClearClick=()=>{
+    let newText="";
+    setText(newText);
+   }
+   
+   
+   
+  
+    const[Text , setText]=useState('');
+  
+    return(
+        <>
+        
+  
+   <div className="mb-3 box-m">
+   <h4 className="my-3">{props.heading}</h4>
+   <textarea className="form-control"  value= {Text} onChange ={HandleOnChange} id="text-box" rows="6" placeholder="Start to Write here...."></textarea>
+   <button className="btn btn-secondary my-4" onClick={UpperClick}>Uppercase</button>
+    <button className="btn btn-secondary mx-3 my-4" onClick={LowerClick}>Lowercase</button>
+    <button className="btn btn-secondary my-4" onClick={ClearClick}>Clear</button>
+   
+
+   <div className="t-summry">
+   <span>{Text.split(" ").length} words and {Text.length} charachters</span>
+   <h3>Preview</h3>
+   <p>{Text}</p>
+   </div>
+   </div>
+  
+   
+
+        </>
+    );
+}
+export default TextForm;
