1cd41f6bf11736a7051fc0f6da07531f8b3911d4
khushboo <khushboobagoria09@gmail.com>
2022-11-20 19:57:22 +0100

    textutils

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -24,7 +24,17 @@ function TextForm(props){
    }
    
    
-   
+   const handleExtraSpaces = ()=>{
+    let words = Text.split(/[\s]+/);
+    let joinedWords=words.join(" ");
+     setText(joinedWords);
+      }
+
+const copyTextHandle=()=>{
+    let seletText= document.getElementById('text-box');
+    seletText.select();
+    navigator.clipboard.writeText(seletText.value);
+}
   
     const[Text , setText]=useState('');
   
@@ -37,6 +47,8 @@ function TextForm(props){
    <textarea className="form-control"  value= {Text} onChange ={HandleOnChange} id="text-box" rows="6" placeholder="Start to Write here...."></textarea>
    <button className="btn btn-secondary my-4" onClick={UpperClick}>Uppercase</button>
     <button className="btn btn-secondary mx-3 my-4" onClick={LowerClick}>Lowercase</button>
+    <button className="btn btn-secondary mx-3 my-4" onClick={handleExtraSpaces}>Remove Spaces</button>
+    <button className="btn btn-secondary mx-3 my-4" onClick={copyTextHandle}>Copy Text</button>
     <button className="btn btn-secondary my-4" onClick={ClearClick}>Clear</button>
    
 
