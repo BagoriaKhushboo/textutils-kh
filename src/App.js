c3c4a9e27015658d7eef1d2e8756160cc892f4b3
khushboo <khushboobagoria09@gmail.com>
2022-11-22 22:51:28 +0100

    darkmode

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TextForm from './components/Textform';
-import Header from './components/Header';
+
 import './components/common.css';
+import { useState } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
+  const[mode , setMode]=useState('light');
+  // const[greenMode , setGreenMode]=useState('light');
+  // const[blueMode , setBlueMode]=useState('light');
+  const[text , setText]=useState('dark mode');
+const toggleMode=()=>{
+  if(mode==="light"){
+    setMode('dark');
+    // setGreenMode('success');
+    // setBlueMode('primary');
+    setText('Light Mode');
+    document.body.style.backgroundColor='#916271';
+    
+  }
+  else{
+    setMode('light');
+    // setGreenMode('light');
+    // setBlueMode('light');
+    setText('Dark Mode');
+    document.body.style.backgroundColor='white';
+  }
+}
+
+
   return (
     <>
    
       <div className="box">
-    <Header title="Textutils" />
-   
-  
-  <TextForm  heading="Enter your Text Here"/>
+  <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} textMode={text} />
+  <TextForm  heading="Enter your Text Here" mode={mode}/>
   </div>
   </>
   );
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,67 +1,90 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
-
-function TextForm(props){
-   
-   const UpperClick =()=>{
-   
+function TextForm(props) {
+  const UpperClick = () => {
     // console.log("uppercase button was clicked");
-   let newText=Text.toUpperCase();
+    let newText = Text.toUpperCase();
     setText(newText);
-   }
-   const LowerClick=()=>{
-    let newText=Text.toLowerCase();
+  };
+  const LowerClick = () => {
+    let newText = Text.toLowerCase();
     setText(newText);
-   }
-   const HandleOnChange =(event)=>{
+  };
+  const HandleOnChange = (event) => {
     // console.log("on change");
-    setText(event.target.value);  
-   
-   }
-   const ClearClick=()=>{
-    let newText="";
+    setText(event.target.value);
+  };
+  const ClearClick = () => {
+    let newText = "";
     setText(newText);
-   }
-   
-   
-   const handleExtraSpaces = ()=>{
-    let words = Text.split(/[\s]+/);
-    let joinedWords=words.join(" ");
-     setText(joinedWords);
-      }
+  };
 
-const copyTextHandle=()=>{
-    let seletText= document.getElementById('text-box');
+  const handleExtraSpaces = () => {
+    let words = Text.split(/[\s]+/);
+    let joinedWords = words.join(" ");
+    setText(joinedWords);
+  };
+
+  const copyTextHandle = () => {
+    let seletText = document.getElementById("text-box");
     seletText.select();
     navigator.clipboard.writeText(seletText.value);
-}
-  
-    const[Text , setText]=useState('');
-  
-    return(
-        <>
-        
-  
-   <div className="mb-3 box-m">
-   <h4 className="my-3">{props.heading}</h4>
-   <textarea className="form-control"  value= {Text} onChange ={HandleOnChange} id="text-box" rows="6" placeholder="Start to Write here...."></textarea>
-   <button className="btn btn-secondary my-4" onClick={UpperClick}>Uppercase</button>
-    <button className="btn btn-secondary mx-3 my-4" onClick={LowerClick}>Lowercase</button>
-    <button className="btn btn-secondary mx-3 my-4" onClick={handleExtraSpaces}>Remove Spaces</button>
-    <button className="btn btn-secondary mx-3 my-4" onClick={copyTextHandle}>Copy Text</button>
-    <button className="btn btn-secondary my-4" onClick={ClearClick}>Clear</button>
-   
+  };
 
-   <div className="t-summry">
-   <span>{Text.split(" ").length} words and {Text.length} charachters</span>
-   <h3>Preview</h3>
-   <p>{Text}</p>
-   </div>
-   </div>
-  
-   
+  const [Text, setText] = useState("");
 
-        </>
-    );
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "light" ? "black" : "white" }}
+      >
+        <div className="mb-3 box-m">
+          <h4 className="my-3">{props.heading}</h4>
+          <textarea
+            className="form-control"
+            value={Text}
+            onChange={HandleOnChange}
+            style={{
+              backgroundColor: props.mode==='dark' ? '#916271':'white',
+              color: props.mode === "dark" ? "white" : "black"
+            }}
+            id="text-box"
+            rows="6"
+            placeholder="Start to Write here...."
+          ></textarea>
+          <button className="btn btn-dark my-4" onClick={UpperClick}>
+            Uppercase
+          </button>
+          <button className="btn btn-dark mx-3 my-4" onClick={LowerClick}>
+            Lowercase
+          </button>
+          <button
+            className="btn btn-dark mx-3 my-4"
+            onClick={handleExtraSpaces}
+          >
+            Remove Spaces
+          </button>
+          <button
+            className="btn btn-dark mx-3 my-4"
+            onClick={copyTextHandle}
+          >
+            Copy Text
+          </button>
+          <button className="btn btn-dark my-4" onClick={ClearClick}>
+            Clear
+          </button>
+
+          <div className="t-summry">
+            <span>
+              {Text.split(" ").length} words and {Text.length} charachters
+            </span>
+            <h3>Preview</h3>
+            <p>{Text.length > 0 ? Text : "write something for preview"}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default TextForm;
